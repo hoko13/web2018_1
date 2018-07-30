ae951790df3a91afc9a5f6da077994ec700bbcaf
zm17419 <zm17419@josai.ac.jp>
2018-07-29 20:57:41 -0400

    message

---
+++ ex6-5.js
@@ -0,0 +1,10 @@
+var who ='world';
+function sayhello(){
+ alert('hello,'+ who + '!');
+ }
+ function taro(){
+ who='太郎';
+ }
+ function hanako(){
+  who='花子';
+ }
---
+++ ox06-7.js
@@ -0,0 +1,11 @@
+var who ='world';
+function sayhello(){
+  alert('Hello,'+who+'!');
+}
+function someone(x){
+ who = x; 
+}
+  
+  
+  
+  
